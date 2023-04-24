a09f26573923ea206dfe27126d5ef25065c080d2
Osama-Hesham <osamahesham982@gmail.com>
2023-04-24 11:15:38 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * _printf - return number of characters
+ * @format: input
+ * Return: output
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+				count += putchar(va_arg(args, int));
+			else if (*format == 's')
+				count += printf("%s", va_arg(args, char *));
+			else if (*format == '%')
+				count += putchar('%');
+		}
+		else
+		{
+			count += putchar(*format);
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
